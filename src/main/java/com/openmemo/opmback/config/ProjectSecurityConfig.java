b6886e51f737b3cd277a9aac2781bd2bbfd03dd2
amagramming-git <knb1031_ba@icloud.com>
2023-12-09 16:32:20 +0900

    追加 jwtによる認証

--- src/main/java/com/openmemo/opmback/config/ProjectSecurityConfig.java
+++ src/main/java/com/openmemo/opmback/config/ProjectSecurityConfig.java
@@ -18,6 +18,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.openmemo.opmback.filter.CsrfCookieFilter;
+import com.openmemo.opmback.filter.JwtTokenGeneratorFilter;
+import com.openmemo.opmback.filter.JwtTokenValidatorFilter;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -27,9 +29,7 @@ public class ProjectSecurityConfig {
         CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
         requestHandler.setCsrfRequestAttributeName("_csrf");
 
-        http.securityContext((context) -> context
-                .requireExplicitSave(false))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
@@ -46,11 +46,13 @@ public class ProjectSecurityConfig {
                         .ignoringRequestMatchers("/customer/register") // csrfの例外を設定
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                 .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+                .addFilterAfter(new JwtTokenGeneratorFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new JwtTokenValidatorFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/admintest").hasRole("ADMIN")
                         .requestMatchers("/anytest").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers("/usertest", "/customer/get", "/post/**").hasRole("USER")
-                        .requestMatchers("/authtest").authenticated() // 認証の必要なリクエスト
+                        .requestMatchers("/usertest", "/post/**").hasRole("USER")
+                        .requestMatchers("/customer/get", "/authtest").authenticated() // 認証の必要なリクエスト
                         .requestMatchers("/customer/register", "/normaltest").permitAll()) // 認証の必要無いリクエスト
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
---
+++ src/main/java/com/openmemo/opmback/filter/JwtTokenValidatorFilter.java
@@ -0,0 +1,57 @@
+package com.openmemo.opmback.filter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.openmemo.opmback.util.ConstantSecurity;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class JwtTokenValidatorFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String jwt = request.getHeader(ConstantSecurity.JWT_HEADER);
+        if (null != jwt) {
+            try {
+                SecretKey key = Keys.hmacShaKeyFor(
+                        ConstantSecurity.JWT_KEY.getBytes(StandardCharsets.UTF_8));
+
+                Claims claims = Jwts.parserBuilder()
+                        .setSigningKey(key)
+                        .build()
+                        .parseClaimsJws(jwt)
+                        .getBody();
+                String username = String.valueOf(claims.get("username"));
+                String authorities = (String) claims.get("authorities");
+                Authentication auth = new UsernamePasswordAuthenticationToken(username, null,
+                        AuthorityUtils.commaSeparatedStringToAuthorityList(authorities));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            } catch (Exception e) {
+                throw new BadCredentialsException("Invalid Token received!");
+            }
+
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return request.getServletPath().equals("/customer/get");
+    }
+
+}
